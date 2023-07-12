dafda39dc15659eb023cc6cf54f331e19666c093
Payam Yavari <payam@yavtech.se>
2023-07-12 10:34:30 +0200

    Use workspace

---
+++ src/libs/blogparser/src/element_ref_extensions.rs
@@ -0,0 +1,16 @@
+use scraper::ElementRef;
+
+pub trait Extensions {
+    fn get_string(self) -> String;
+    fn get_url(self) -> Option<String>;
+}
+
+impl Extensions for ElementRef<'_> {
+    fn get_string(self) -> String {
+        self.text().collect::<Vec<_>>().join("")
+    }
+
+    fn get_url(self) -> Option<String> {
+        self.value().attr("href").map(|e| e.to_string())
+    }
+}
---
+++ src/rssreader/src/main.rs
@@ -0,0 +1,51 @@
+use blogparser::BlogParser::{Blog, ArticleInstruction, BlogIndex, ParseInstruction, Order};
+use reqwest::Client;
+
+#[tokio::main]
+async fn main() {
+    let client: Client = Client::new();
+
+    let blogs = vec![
+        Blog {
+            client: client.clone(),
+            url: "https://smallcultfollowing.com".to_string(),
+            url_suffix: Some("babysteps".to_string()),
+            index: BlogIndex {
+                section: ParseInstruction::Selectors(".content_body".to_string(), Order::Normal(0)),
+                link_selector: ParseInstruction::Selectors(".post_section".to_string(), Order::Normal(0)),
+            },
+            article: ArticleInstruction {
+                section: ParseInstruction::Selectors("article".to_string(), Order::Normal(0)),
+                headline: ParseInstruction::Selectors("h1".to_string(), Order::Normal(0)),
+                content: Some(ParseInstruction::Selectors(".post-content".to_string(), Order::Normal(0))),
+                date: Some(ParseInstruction::Selectors("time".to_string(), Order::Reverse(0))),
+            },
+        },
+        Blog {
+            client: client.clone(),
+            url: "https://payam.yavari.se".to_string(),
+            url_suffix: None,
+            index: BlogIndex {
+                section: ParseInstruction::Selectors(".col-md-10".to_string(), Order::Normal(0)),
+                link_selector: ParseInstruction::Selectors("li".to_string(), Order::Normal(0)),
+            },
+            article: ArticleInstruction {
+                section: ParseInstruction::Selectors(".col-md-10".to_string(), Order::Normal(0)),
+                headline: ParseInstruction::Selectors("h1".to_string(), Order::Normal(0)),
+                content: None,
+                date: None,
+            },
+        },
+    ];
+
+    for blog in blogs {
+        let response = blog.fetch_blog().await;
+        let urls = blog.parse_links(&response);
+        println!("{:?}", urls);
+        for url in urls {
+            let html = blog.fetch_article(&url).await;
+            println!("{:?}", blog.parse_article(&html));
+            return;
+        }
+    }
+}
